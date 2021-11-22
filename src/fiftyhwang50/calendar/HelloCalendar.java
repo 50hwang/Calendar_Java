d590df51ab380c496be44ca990bf0f06f561042c
Sooin Hwang <Sooin Hwang@192.168.219.101>
2021-11-22 16:38:24 +0900

    Slight change of the output message.

--- src/fiftyhwang50/calendar/HelloCalendar.java
+++ src/fiftyhwang50/calendar/HelloCalendar.java
@@ -3,7 +3,7 @@ package fiftyhwang50.calendar;
 public class HelloCalendar {
 		
 	public static void main(String args[]) {
-		System.out.println("Hello, Calendar");
+		System.out.println("Hello, Calendar.");
 	}
 
 }

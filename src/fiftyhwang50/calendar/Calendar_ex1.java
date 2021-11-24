785621e25cb25c315a94f1a8b3605e83f0464cb2
Sooin Hwang <Sooin Hwang@192.168.219.106>
2021-11-24 18:28:01 +0900

    Added a single program that indicated max days of the certain month.
     - Used user input.
     - Used max days data as an integer array.

---
+++ src/fiftyhwang50/calendar/Calendar_ex1.java
@@ -0,0 +1,16 @@
+package fiftyhwang50.calendar;
+
+import java.util.Scanner;
+
+public class Calendar_ex1 {
+	public static void main(String[] args) {
+		//숫자를 입력받아 해당하는 달의 최대 일 수를 출력받는 프로그램 
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하시오 : ");
+		int month = scanner.nextInt();
+		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 31};	//월별 최대 일수를 정수형 배열로 지정
+		System.out.printf("%d월은 %d일까지 있습니다.\n", month, maxDays[month - 1]);
+			//윤년의 경우까지 고려하지는 않음.
+		scanner.close();
+	}
+}
